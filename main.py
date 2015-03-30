96d4cf6e37c7119fb6f7747fb554b707659d96ea
Jeff Mendoza <jeffmendoza@google.com>
2015-03-30 11:14:48 -0700

    Add tags for docs.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # Copyright 2015 Google Inc. All rights reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START all]
 import webapp2
 
 from google.appengine.api import users
@@ -29,13 +30,18 @@ class UserPhoto(ndb.Model):
 
 class PhotoUploadFormHandler(webapp2.RequestHandler):
     def get(self):
+        # [START upload_url]
         upload_url = blobstore.create_upload_url('/upload_photo')
+        # [END upload_url]
+        # [START upload_form]
         # The method must be "POST" and enctype must be set to "multipart/form-data".
         self.response.out.write('<html><body>')
         self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
         self.response.out.write('''Upload File: <input type="file" name="file"><br> <input type="submit"
             name="submit" value="Submit"> </form></body></html>''')
+        # [END upload_form]
 
+# [START upload_handler]
 class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         try:
@@ -48,16 +54,20 @@ class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
 
         except:
             self.redirect('/upload_failure .html')
+# [END upload_handler]
 
+# [START download_handler]
 class ViewPhotoHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, photo_key):
         if not blobstore.get(photo_key):
             self.error(404)
         else:
             self.send_blob(photo_key)
+# [END download_handler]
 
 
 app = webapp2.WSGIApplication([('/', PhotoUploadFormHandler),
                                ('/upload_photo', PhotoUploadHandler),
                                ('/view_photo/([^/]+)?', ViewPhotoHandler),
                               ], debug=True)
+# [END all]
